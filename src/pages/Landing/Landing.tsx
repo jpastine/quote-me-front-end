4cd7b8dbce6f83c894459403ace32314ffddc480
justinpastine <justin.pastine@gmail.com>
2023-03-01 09:10:33 -0500

    Add to landing page

--- src/pages/Landing/Landing.tsx
+++ src/pages/Landing/Landing.tsx
@@ -14,6 +14,8 @@ const Landing = (props: LandingProps): JSX.Element => {
   return (
     <main className={styles.container}>
       <h1>Hello, {user ? user.name : 'friend'}</h1>
+      <h1>Welcome to QuoteMe</h1>
+      <h2>A place for uplifting, moving, and funny quotes!</h2>
     </main>
   )
 }
