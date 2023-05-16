3f551c583a465905e2ed460cdd18e70d50804d51
justinpastine <justin.pastine@gmail.com>
2023-05-16 14:42:39 -0400

    correct typos

--- src/pages/Landing/Landing.tsx
+++ src/pages/Landing/Landing.tsx
@@ -17,8 +17,8 @@ const Landing = (props: LandingProps): JSX.Element => {
       <h1>Welcome to <div id='title' className={styles.landing}> QuoteMe</div></h1>
       <h2>A place for uplifting, moving, and funny quotes!</h2>
       <div className={styles.landingContainer}>
-        <div className={styles.instructions}>1. Log in or sign up to get started! Then add your favorite quote and author</div>
-        <div className={styles.instructions}>2. Check out other quotes that have been added and give the a rating.</div>
+        <div className={styles.instructions}>1. Log in or sign up to get started! Then add your favorite quote and author.</div>
+        <div className={styles.instructions}>2. Check out other quotes that have been added and give them a rating.</div>
         <div className={styles.instructions}>3. Come back daily for inspiration or laughs!</div>
       </div>
     </main>
